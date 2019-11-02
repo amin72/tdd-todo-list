066f131bd16216cb6f04fee443e3557f7c420376
Amin <asaa47721@gmail.com>
2019-11-02 16:53:06 +0330

    remove code for creating new items from home page

--- lists/views.py
+++ lists/views.py
@@ -4,14 +4,8 @@ from .models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('lists:list_view')
-    else:
-        items = Item.objects.all()
-        return render(request, 'lists/home.html',
-            {'items': items})
+    items = Item.objects.all()
+    return render(request, 'lists/home.html', {'items': items})
 
 
 def list_view(request):
